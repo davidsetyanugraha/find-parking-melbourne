14c50f1730ef31dd7fc285b95b9b82b49055caa5
Miguel Zapata <16281012+MigueZS@users.noreply.github.com>
2019-09-22 18:41:24 +1000

    Import availability skeleton

--- Api/SitesStateImport.cs
+++ Api/SitesStateImport.cs
@@ -3,31 +3,34 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.Azure.Documents.Spatial;
 using Microsoft.Azure.WebJobs;
-// using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
 
-namespace Unimelb.Parking
+namespace Parking.Sites
 {
-    public static class ParkingAvailabilitySample
+    public static class SitesStateImport
     {
         static HttpClient client = new HttpClient();
         static string parkingApiUrl = "https://data.melbourne.vic.gov.au/resource/vh2v-4nfs.json?%24limit=10000";
         
-        [FunctionName("ParkingAvailabilitySample")]
+        [FunctionName("SitesStateImport")]
         public static async Task RunAsync(
-            [TimerTrigger("0 */2 * * * *")]TimerInfo myTimer,
+            //[TimerTrigger("0 */2 * * * *")]TimerInfo myTimer,
+            [HttpTrigger(Microsoft.Azure.WebJobs.Extensions.Http.AuthorizationLevel.Anonymous, "post", Route = "sites/stateupdate")] Microsoft.AspNetCore.Http.HttpRequest req,
             [CosmosDB(
                 databaseName: "parkingdb",
-                collectionName: "spots",
+                collectionName: "sitesstate",
                 ConnectionStringSetting = "CosmosDBConnectionString")]IAsyncCollector<dynamic> documents,
             ILogger log)
         {
-            log.LogInformation($"C# Timer trigger function executed at: {DateTime.Now}");
+            log.LogInformation($"Sites State import executed at: {DateTime.Now}");
 
             using (HttpResponseMessage res = await client.GetAsync(parkingApiUrl))
+            {
                 using (HttpContent content = res.Content)
                 {
+                    var lastUpdate = DateTime.Now;
                     dynamic result = JsonConvert.DeserializeObject(await content.ReadAsStringAsync());
                     foreach (var item in result) {
                         var newObject = new {
@@ -36,13 +39,15 @@ namespace Unimelb.Parking
                             location = new Point(
                                 (double)item.location.longitude,
                                 (double)item.location.latitude
-                            )
+                            ),
+                            lastUpdate = lastUpdate
                         };
                         await documents.AddAsync(newObject);
                     }
 
-                    log.LogInformation($"Total parkings processed {result.Count}");
+                    log.LogInformation($"Total sites processed {result.Count}");
                 }
+            }
         }
     }
 }
