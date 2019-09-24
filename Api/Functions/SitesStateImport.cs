d552c2818e8de886764fc42c56fbb0b7d9a0c1c3
Miguel Zapata <16281012+MigueZS@users.noreply.github.com>
2019-09-24 15:18:13 +1000

    Import state allow updates and deletions

--- Api/Functions/SitesStateImport.cs
+++ Api/Functions/SitesStateImport.cs
@@ -1,7 +1,12 @@
 using System;
+using System.Collections.Generic;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Api.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Documents;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents.Linq;
 using Microsoft.Azure.Documents.Spatial;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
@@ -12,43 +17,89 @@ namespace Api.Functions
 {
     public static class SitesStateImport
     {
-        static HttpClient client = new HttpClient();
-        static string parkingApiUrl = "https://data.melbourne.vic.gov.au/resource/vh2v-4nfs.json?%24limit=10000";
+        static HttpClient httpClient = new HttpClient();
+        static string parkingApiUrl = "https://data.melbourne.vic.gov.au/resource/vh2v-4nfs.json?%24limit=10";
         
         [FunctionName("SitesStateImport")]
         public static async Task<IActionResult> RunAsync(
             //[TimerTrigger("0 */2 * * * *")]TimerInfo myTimer,
             [HttpTrigger(Microsoft.Azure.WebJobs.Extensions.Http.AuthorizationLevel.Anonymous, "post", Route = "sites/state/import")] Microsoft.AspNetCore.Http.HttpRequest req,
-            [CosmosDB(
-                databaseName: "parkingdb",
-                collectionName: "sitesstate",
-                ConnectionStringSetting = "CosmosDBConnectionString")]IAsyncCollector<dynamic> documents,
+            // [CosmosDB(
+            //     databaseName: "parkingdb",
+            //     collectionName: "sitesstate",
+            //     ConnectionStringSetting = "CosmosDBConnectionString")]IEnumerable<SiteState> originalSites,
+            // [CosmosDB(
+            //     databaseName: "parkingdb",
+            //     collectionName: "sitesstate",
+            //     ConnectionStringSetting = "CosmosDBConnectionString")]IAsyncCollector<SiteState> destinationSites,
+            [CosmosDB(ConnectionStringSetting = "CosmosDBConnectionString")] DocumentClient documentClient,
             ILogger log)
         {
             log.LogInformation($"Sites State import starting at: {DateTime.Now}");
 
-            using (HttpResponseMessage res = await client.GetAsync(parkingApiUrl))
+            Uri collectionUri = UriFactory.CreateDocumentCollectionUri("parkingdb", "sitesstate");
+            IDocumentQuery<SiteState> query = documentClient.CreateDocumentQuery<SiteState>(collectionUri,
+               new FeedOptions() { PartitionKey = new PartitionKey(null) })
+               .AsDocumentQuery();
+            var oldSites = await query.ToDictionary(x => x.Id);
+
+            // return new ObjectResult(oldSites);
+            log.LogInformation($"Sites State import old records imported at: {DateTime.Now}");
+
+            var newSites = new List<SiteState>();
+
+            using (HttpResponseMessage res = await httpClient.GetAsync(parkingApiUrl))
             {
-                log.LogInformation($"Sites State import origin downloaded at: {DateTime.Now}");
                 using (HttpContent content = res.Content)
                 {
                     var lastUpdate = DateTime.UtcNow;
                     dynamic result = JsonConvert.DeserializeObject(await content.ReadAsStringAsync());
+
+                    log.LogInformation($"Sites State import origin downloaded at: {DateTime.Now}. Total sites {result.Count}");
+
                     foreach (var item in result) {
-                        var newObject = new {
-                            id = item.bay_id.ToString(),
-                            status = item.status,
-                            location = new Point(
+                        var newSite = new SiteState()
+                        {
+                            Id = item.bay_id.ToString(),
+                            Status = item.status,
+                            Location = new Point(
                                 (double)item.location.longitude,
                                 (double)item.location.latitude
                             ),
-                            lastUpdate = lastUpdate
+                            // LastUpdate = lastUpdate
                         };
-                        await documents.AddAsync(newObject);
+                        
+                        SiteState oldSite;
+                        
+                        if (!oldSites.TryGetValue(newSite.Id, out oldSite))
+                        {
+                            newSites.Add(newSite);
+                        }
+                        else
+                        {
+                            oldSites.Remove(newSite.Id);
+                            if (oldSite.Status != newSite.Status) //|| !oldSite.Location.Equals(newSite.Location))
+                            {
+                                newSites.Add(newSite);
+                            }
+                        }
                     }
-
-                    log.LogInformation($"Sites State import finished at {DateTime.Now}. Total sites processed {result.Count}");
                 }
+            }
+
+            log.LogInformation($"Sites State import processed at: {DateTime.Now}. Sites to upsert {newSites.Count}. Sites to delete {oldSites.Count}");
+
+            foreach (var item in newSites)
+            {
+                await documentClient.UpsertDocumentAsync(collectionUri, item);
+                //await destinationSites.AddAsync(newObject);
+            }
+
+            foreach (var keyPair in oldSites)
+            {
+                await documentClient.DeleteDocumentAsync(
+                    UriFactory.CreateDocumentUri("parkingdb", "sitesstate", keyPair.Key),
+                    new RequestOptions() { PartitionKey = new PartitionKey(null) });
             }
 
             return new NoContentResult();
