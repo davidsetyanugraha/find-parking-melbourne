6d6e7642518fae833108d461d21f0f64cebe816e
919750 <iqbalw@student.unimelb.edu.au>
2019-09-24 19:00:51 +1000

    AutoCompleteSearch Implemented

--- AndroidApp/app/src/main/java/com/unimelbs/parkingassistant/MapsActivity.java
+++ AndroidApp/app/src/main/java/com/unimelbs/parkingassistant/MapsActivity.java
@@ -3,27 +3,95 @@ package com.unimelbs.parkingassistant;
 import androidx.fragment.app.FragmentActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
-import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.common.api.Status;
+import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.net.PlacesClient;
+import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
+import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
+import com.google.android.libraries.places.api.model.Place;
+
+import java.util.Arrays;
+import java.util.List;
+
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
+    public static String apiKey;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+
+        initializeGoogleMapsPlacesApis();
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        activateAutoCompleteFragment();
+
+
+    }
+
+    private void activateAutoCompleteFragment(){
+
+        List<Place.Field> placeFields = Arrays.asList(Place.Field.ID, Place.Field.NAME,Place.Field.LAT_LNG);
+
+        AutocompleteSupportFragment autocompleteFragment = (AutocompleteSupportFragment)
+                getSupportFragmentManager().findFragmentById(R.id.autocomplete_fragment);
+
+
+
+        try {
+            autocompleteFragment.setPlaceFields(placeFields);
+        } catch (NullPointerException e) {
+            Log.d("NP", "Null Pointer Exception while setting Place Fields to Retrieve");
+            e.printStackTrace();
+        }
+
+        // Set up a PlaceSelectionListener to handle the response.
+        autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
+            @Override
+            public void onPlaceSelected(Place place) {
+
+                String placeName = place.getName();
+                LatLng placeLatLng = place.getLatLng();
+
+
+                mMap.addMarker(new MarkerOptions().position(placeLatLng).title(placeName));
+                mMap.moveCamera(CameraUpdateFactory.newLatLng(placeLatLng));
+                mMap.moveCamera(CameraUpdateFactory.zoomTo(18));
+                Log.i("LocationSetTo", "Place: " + placeName + ", " + place.getId()+ ", " + placeLatLng);
+            }
+
+            @Override
+            public void onError(Status status) {
+                // TODO: Handle the error.
+                Log.i("onError", "An error occurred: " + status);
+            }
+        });
+
+    }
+
+    private void initializeGoogleMapsPlacesApis(){
+
+        MapsActivity.apiKey = getResources().getString(R.string.api_key_googlemaps);
+        // Initialize the Places SDK
+        Places.initialize(getApplicationContext(), apiKey);
+        // Create a new Places client instance
+        PlacesClient placesClient = Places.createClient(this);
+
     }
 
 
