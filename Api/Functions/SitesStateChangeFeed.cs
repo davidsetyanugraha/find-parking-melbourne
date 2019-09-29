dd7c00331d464a6e722b4a2e328c28816b74064b
Miguel Zapata <16281012+MigueZS@users.noreply.github.com>
2019-09-29 13:33:08 +1000

    Publishing changes to all clients (w html sample)

--- Api/Functions/SitesStateChangeFeed.cs
+++ Api/Functions/SitesStateChangeFeed.cs
@@ -1,31 +1,44 @@
 using System;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using Microsoft.Azure.Documents;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Azure.WebJobs.Extensions.SignalRService;
 using Microsoft.Extensions.Logging;
 
 namespace Api.Functions
 {
+    // Flights example https://davetheunissen.io/real-time-flight-map-w-azure-functions-cosmosdb-signalr/
+    //https://anthonychu.ca/post/cosmosdb-real-time-azure-functions-signalr-service/
+    //https://www.serverless360.com/blog/build-real-time-serverless-application-with-azure-functions-and-signalr
+    //http://dontcodetired.com/blog/post/Using-the-Azure-SignalR-Service-Bindings-in-Azure-Functions-to-Create-Real-time-Serverless-Applications
+    //https://docs.microsoft.com/en-us/azure/azure-signalr/signalr-quickstart-azure-functions-csharp
     public static class SitesStateChangeFeed
     {
-        static int count = 0;
-
         [FunctionName("SitesStateChangeFeed")]
-        public static void Run([CosmosDBTrigger(
-            databaseName: "parkingdb",
-            collectionName: "sitesstate",
-            ConnectionStringSetting = "CosmosDBConnectionString",
-            LeaseCollectionName = "leases",
-            CreateLeaseCollectionIfNotExists = true)]IReadOnlyList<Document> input, ILogger log)
+        public static Task Run(
+            [CosmosDBTrigger(
+                databaseName: "parkingdb",
+                collectionName: "sitesstate",
+                ConnectionStringSetting = "CosmosDBConnectionString",
+                LeaseCollectionName = "leases",
+                //FeedPollDelay = 1000,
+                CreateLeaseCollectionIfNotExists = true)]IReadOnlyList<Document> input,
+            [SignalR(HubName = "SitesState")]IAsyncCollector<SignalRMessage> signalRMessages,
+            ILogger log)
         {
             if (input != null && input.Count > 0)
             {
                 log.LogInformation("Documents modified " + input.Count);
-                log.LogInformation("First document Id " + input[0].Id);
-                count+=input.Count;
-                log.LogInformation("Total Documents modified " + count);
             }
+
+            return signalRMessages.AddAsync(
+                new SignalRMessage 
+                {
+                    Target = "SitesState", 
+                    Arguments = new [] { input } 
+                });
         }
     }
 }
