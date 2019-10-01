2dc558196d066bd6acb4f6f5915a42ecac9d75b7
Miguel Zapata <16281012+MigueZS@users.noreply.github.com>
2019-10-01 16:46:24 +1000

    Update status function

---
+++ Api/Functions/SitesStateUpdate.cs
@@ -0,0 +1,61 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using FluentValidation;
+using Api.Models;
+using Microsoft.Azure.Documents.Client;
+using Microsoft.Azure.Documents;
+
+namespace Api.Functions
+{
+    public static class SitesStateUpdate
+    {
+        public class Command
+        {
+            public string Id { get; set; }
+            public string Status { get; set; }
+        }
+
+        public class CommandValidator : AbstractValidator<Command>
+        {
+            public CommandValidator()
+            {
+                RuleFor(x => x.Id)
+                    .NotEmpty();
+                RuleFor(x => x.Status)
+                    .NotNull();
+            }
+        }
+
+        //https://markheath.net/post/azure-functions-rest-csharp-bindings
+        [FunctionName("SitesStateUpdate")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "put", Route = "sites/state/{id}")] Command command,
+            [CosmosDB(ConnectionStringSetting = "CosmosDBConnectionString")] DocumentClient client,
+            ILogger log)
+        {
+            Uri collectionUri = UriFactory.CreateDocumentCollectionUri("parkingdb", "sitesstate");
+
+            var document = client.CreateDocumentQuery(collectionUri,
+                new FeedOptions() { PartitionKey = new PartitionKey(null) })
+                .Where(x => x.Id == command.Id)
+                .AsEnumerable().FirstOrDefault();
+            if (document == null)
+            {
+                return new NotFoundResult();
+            }
+            
+            document.SetPropertyValue("status", command.Status);
+            await client.ReplaceDocumentAsync(document);
+
+            return new OkObjectResult(document);
+        }
+    }
+}
