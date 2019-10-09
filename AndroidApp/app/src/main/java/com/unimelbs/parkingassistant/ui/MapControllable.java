ec754e62fadc96e52c3690d3c5f97b440625a316
Tarek Elbeik <elbeikt@student.unimelb.edu.au>
2019-10-09 15:05:20 +1100

    Added DataFeed, MapControllable, MapPresenter, CustomClusterManager classes.

---
+++ AndroidApp/app/src/main/java/com/unimelbs/parkingassistant/ui/MapControllable.java
@@ -0,0 +1,7 @@
+package com.unimelbs.parkingassistant.ui;
+
+import com.google.android.gms.maps.model.CameraPosition;
+
+public interface MapControllable {
+    CameraPosition getCameraPosition();
+}
