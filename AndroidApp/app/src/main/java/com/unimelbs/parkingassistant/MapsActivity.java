553a471aa72b9053caa1778947041257aee619f5
davidsetyanugraha <dsetyanugraha@gmail.com>
2019-10-10 17:17:26 +1100

    Add Bottom Sheet Impl, ButterKnife dependency, Material, AndroidX

--- AndroidApp/app/src/main/java/com/unimelbs/parkingassistant/MapsActivity.java
+++ AndroidApp/app/src/main/java/com/unimelbs/parkingassistant/MapsActivity.java
@@ -1,41 +1,57 @@
 package com.unimelbs.parkingassistant;
 
-import androidx.fragment.app.FragmentActivity;
-
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
+import com.google.android.gms.common.api.Status;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.common.api.Status;
 import com.google.android.libraries.places.api.Places;
+import com.google.android.libraries.places.api.model.Place;
 import com.google.android.libraries.places.api.net.PlacesClient;
 import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
 import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
-import com.google.android.libraries.places.api.model.Place;
+import com.google.android.material.bottomsheet.BottomSheetBehavior;
+import com.unimelbs.parkingassistant.model.Bay;
+import com.unimelbs.parkingassistant.model.DataFeed;
+import com.unimelbs.parkingassistant.model.ExtendedClusterManager;
+import com.unimelbs.parkingassistant.util.PermissionManager;
 
 import java.util.Arrays;
 import java.util.List;
 
-import com.unimelbs.parkingassistant.model.Bay;
-import com.unimelbs.parkingassistant.model.DataFeed;
-import com.unimelbs.parkingassistant.model.ExtendedClusterManager;
-import com.unimelbs.parkingassistant.util.*;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private static final String TAG = "TEPA";
     private static String apiKey;
 
+    @BindView(R.id.btn_bottom_sheet)
+    Button btnBottomSheet;
+
+    @BindView(R.id.bottom_sheet)
+    LinearLayout layoutBottomSheet;
+
+    BottomSheetBehavior sheetBehavior;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        ButterKnife.bind(this);
 
 
         initializeGoogleMapsPlacesApis();
@@ -48,7 +64,50 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         activateAutoCompleteFragment();
 
+        //bottom sheet behavior
+        sheetBehavior = BottomSheetBehavior.from(layoutBottomSheet);
 
+        /**
+         * bottom sheet state change listener
+         * we are changing button text when sheet changed state
+         * */
+        sheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                switch (newState) {
+                    case BottomSheetBehavior.STATE_HIDDEN:
+                    case BottomSheetBehavior.STATE_DRAGGING:
+                    case BottomSheetBehavior.STATE_SETTLING:
+                        break;
+                    case BottomSheetBehavior.STATE_EXPANDED: {
+                        btnBottomSheet.setText("Close Bay");
+                    }
+                    break;
+                    case BottomSheetBehavior.STATE_COLLAPSED: {
+                        btnBottomSheet.setText("Click Bay");
+                    }
+                    break;
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+
+            }
+        });
+
+    }
+
+    /**
+     * manually opening / closing bottom sheet on button click
+     */
+    @OnClick(R.id.btn_bottom_sheet)
+    public void toggleBottomSheet() {
+        if (sheetBehavior.getState() != BottomSheetBehavior.STATE_EXPANDED) {
+            sheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+        } else {
+            sheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+        }
     }
 
     private void activateAutoCompleteFragment(){
