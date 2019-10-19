9e1d5c23d74e0f827745ccd7778e5e96ea71517f
919750 <iqbalw@student.unimelb.edu.au>
2019-10-19 18:15:22 +1100

    Notification And Navigation Completed

--- AndroidApp/app/src/main/java/com/unimelbs/parkingassistant/MapsActivity.java
+++ AndroidApp/app/src/main/java/com/unimelbs/parkingassistant/MapsActivity.java
@@ -1,19 +1,18 @@
 package com.unimelbs.parkingassistant;
 
 import android.app.AlertDialog;
-import android.app.Notification;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
-import android.app.PendingIntent;
+
 import android.content.ActivityNotFoundException;
+import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
-import android.media.RingtoneManager;
+import android.content.ServiceConnection;
+
 import android.net.ConnectivityManager;
 import android.net.Uri;
-import android.os.Build;
+
 import android.os.Bundle;
+import android.os.IBinder;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -21,6 +20,9 @@ import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -36,11 +38,12 @@ import com.google.android.libraries.places.widget.AutocompleteSupportFragment;
 import com.google.android.libraries.places.widget.listener.PlaceSelectionListener;
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 import com.google.maps.android.clustering.ClusterManager;
-import com.uber.autodispose.android.lifecycle.AndroidLifecycleScopeProvider;
+
 import com.unimelbs.parkingassistant.model.Bay;
 import com.unimelbs.parkingassistant.model.DataFeed;
 import com.unimelbs.parkingassistant.model.ExtendedClusterManager;
-import com.unimelbs.parkingassistant.parkingapi.ParkingSiteFollower;
+
+import com.unimelbs.parkingassistant.util.BayUpdateService;
 import com.unimelbs.parkingassistant.util.PermissionManager;
 import com.unimelbs.parkingassistant.util.RestrictionsHelper;
 
@@ -50,9 +53,6 @@ import org.jetbrains.annotations.NotNull;
 import java.util.Arrays;
 import java.util.List;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.NotificationCompat;
-import androidx.lifecycle.Lifecycle;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
@@ -72,10 +72,9 @@ public class MapsActivity extends AppCompatActivity
     private static final String TAG = "MapActivity";
     private static String apiKey;
     private Bay selectedBay;
-    // This is the Notification Channel ID.
-    public static final String NOTIFICATION_CHANNEL_ID = "channel_id";
-    //User visible Channel Name
-    public static final String CHANNEL_NAME = "Notification Channel";
+
+    BayUpdateService bayUpdateService;
+    boolean bayUpdateServiceBound = false;
 
 
     //Bottom sheet and StartParking impl
@@ -125,6 +124,45 @@ public class MapsActivity extends AppCompatActivity
         initBottomSheetUI();
     }
 
+    /** Defines callbacks for service binding, passed to bindService() */
+    private ServiceConnection connection = new ServiceConnection() {
+
+        @Override
+        public void onServiceConnected(ComponentName className, IBinder service) {
+            // We've bound to LocalService, cast the IBinder and get LocalService instance
+            BayUpdateService.bayUpdateServiceBinder binder = (BayUpdateService.bayUpdateServiceBinder) service;
+            bayUpdateService = binder.getService();
+            bayUpdateServiceBound = true;
+        }
+
+        @Override
+        public void onServiceDisconnected(ComponentName arg0) {
+
+            bayUpdateService.disposeSubscription();
+            bayUpdateServiceBound = false;
+        }
+    };
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        // Bind to BayUpdateService
+        Intent intent = new Intent(this, BayUpdateService.class);
+        bindService(intent, connection, Context.BIND_AUTO_CREATE);
+    }
+
+    @Override
+    protected void onDestroy() { //should not unbind the service onStop
+                                 // otherwise it wont interact with server.
+
+        bayUpdateService.disposeSubscription();
+        unbindService(connection);
+        bayUpdateServiceBound = false;
+        super.onDestroy();
+    }
+
+
+
     private void initBottomSheetUI() {
         sheetBehavior = BottomSheetBehavior.from(layoutBottomSheet);
         sheetBehavior.setState(BottomSheetBehavior.STATE_HIDDEN);
@@ -167,7 +205,7 @@ public class MapsActivity extends AppCompatActivity
 
 
             try {
-                subscribeToServerForUpdates(this.selectedBay);
+                bayUpdateService.subscribeToServerForUpdates(this.selectedBay);
                 startActivity(mapIntent);
 
             } catch (ActivityNotFoundException ex) {
@@ -377,97 +415,5 @@ public class MapsActivity extends AppCompatActivity
 
         return result;
     }
-
-
-    private void subscribeToServerForUpdates(@NotNull Bay selectedBay) {
-
-        CompositeDisposable disposable = new CompositeDisposable();
-
-        //Connect the follower to a parking bay
-        ParkingSiteFollower follower = ParkingSiteFollower.getInstance();
-        Disposable d = follower.createParkingBayFollower(Integer.toString(selectedBay.getBayId()))
-                .subscribeOn(Schedulers.io())
-                .observeOn(AndroidSchedulers.mainThread()) // to return to the main thread
-                .as(autoDisposable(AndroidLifecycleScopeProvider.from(this, Lifecycle.Event.ON_STOP))) //to dispose when the activity finishes
-                .subscribe(bay -> {
-                            //Log.v("Notification", "Value:" + bay.getId() + " status of the bay is: " + bay.getStatus()); // sample, other values are id, status, location, zone, recordState
-                            bayStatusChangeNotification();
-                        },
-                        throwable -> Log.d("debug", throwable.getMessage()), // do this on error
-                        () -> Log.d("debug", "complete"));
-
-        //Dispose when disposing the service or when not needed
-        //disposable.add(d);
-
-    }
-
-    public void bayStatusChangeNotification() {
-
-
-        final int NOTIFICATION_ID = 101;
-        String title = "Bay Status Changed";
-        String subject = "Parking Bay Status Changed ";
-        String body = "The status of selected bay has been changed, Tap to Redirect to app";
-        NotificationManager notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
-        // Importance applicable to all the notifications in this Channel
-
-
-        //Notification channel should only be created for devices running Android 26
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            int importance = NotificationManager.IMPORTANCE_HIGH;
-
-            NotificationChannel notificationChannel = new NotificationChannel(NOTIFICATION_CHANNEL_ID, CHANNEL_NAME, importance);
-            //Boolean value to set if lights are enabled for Notifications from this Channel
-            notificationChannel.enableLights(true);
-            //Boolean value to set if vibration are enabled for Notifications from this Channel
-            notificationChannel.enableVibration(true);
-            //Sets the color of Notification Light
-            notificationChannel.setLightColor(Color.GREEN);
-            //Set the vibration pattern for notifications. Pattern is in milliseconds with the format {delay,play,sleep,play,sleep...}
-            notificationChannel.setVibrationPattern(new long[]{
-                    500,
-                    500,
-                    500,
-                    500,
-                    500
-            });
-            //Sets whether notifications from these Channel should be visible on Lockscreen or not
-            notificationChannel.setLockscreenVisibility(Notification.VISIBILITY_PUBLIC);
-            notificationManager.createNotificationChannel(notificationChannel);
-
-        }
-
-        // Notification Channel ID passed
-        // as a parameter here will be
-        // ignored for all the
-        // Android versions below 8.0
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(this, NOTIFICATION_CHANNEL_ID);
-        builder.setContentTitle(title);
-        builder.setContentText(body);
-        builder.setSmallIcon(R.drawable.ic_launcher_background);
-        builder.setPriority(NotificationCompat.PRIORITY_MAX);
-        builder.setVisibility(NotificationCompat.VISIBILITY_PUBLIC);
-        builder.setSound(RingtoneManager.getDefaultUri(RingtoneManager.TYPE_ALARM));
-
-        Intent intent = new Intent(this, MapsActivity.class);
-
-        new Intent(this, MapsActivity.class);
-        PendingIntent pendingIntent = PendingIntent.getActivity(this, 1001, intent, 0);
-        //Following will set the tap action
-        builder.setContentIntent(pendingIntent);
-
-
-        //PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 1002, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-        //builder.addAction(R.drawable.ic_launcher_background, "Redirect to Nearest Bay", pendingIntent);
-        //builder.setLargeIcon(BitmapFactory.decodeResource(getResources(), R.drawable.icon));
-        Notification notification = builder.build();
-
-
-        notificationManager.notify(NOTIFICATION_ID, notification);
-
-
-        Toast.makeText(this, "Selected Bay Status Has Been Changed", Toast.LENGTH_LONG).show();
-    }
-
 
 }
