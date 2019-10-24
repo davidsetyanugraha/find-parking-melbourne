429677ff566c7d38d327a388d89f2df52bfd1411
Miguel Zapata <16281012+MigueZS@users.noreply.github.com>
2019-10-24 15:41:18 +1100

    URL to constants

--- AndroidApp/app/src/main/java/com/unimelbs/parkingassistant/parkingapi/ParkingSiteFollower.java
+++ AndroidApp/app/src/main/java/com/unimelbs/parkingassistant/parkingapi/ParkingSiteFollower.java
@@ -5,6 +5,7 @@ import android.util.Log;
 import com.microsoft.signalr.HubConnection;
 import com.microsoft.signalr.HubConnectionBuilder;
 import com.microsoft.signalr.HubConnectionState;
+import com.unimelbs.parkingassistant.util.Constants;
 
 import io.reactivex.Observable;
 import io.reactivex.Single;
@@ -12,8 +13,6 @@ import io.reactivex.disposables.CompositeDisposable;
 import io.reactivex.disposables.Disposable;
 
 public class ParkingSiteFollower {
-    private static final String connectionUrl = "https://parkingappapi.azurewebsites.net/api/sites/state/connection/";
-//    private static final String connectionUrl = "http://10.8.8.8:7071/api/sites/state/connection/";
 
     private static ParkingSiteFollower instance;
     private ParkingApi api;
@@ -114,7 +113,7 @@ public class ParkingSiteFollower {
     }
 
     private InnerHubConnection createHub() {
-        HubConnection hubConnection = HubConnectionBuilder.create(connectionUrl)
+        HubConnection hubConnection = HubConnectionBuilder.create(Constants.HUB_CONNECTION_URL)
                 .build();
 
         return new InnerHubConnection(hubConnection);
